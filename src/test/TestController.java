321805f67642ae64781f09fc552b876cc2780ea4
LJN <1457888782@qq.com>
2017-03-29 19:04:10 +0800

    Can receive Verfication code from any Province Verification code Ok

---
+++ src/test/TestController.java
@@ -0,0 +1,19 @@
+package test;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by SPREADTRUM\jiannan.liu on 17-3-29.
+ */
+public class TestController implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+// TODO
+    }
+}
